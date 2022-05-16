1f335c986a9787f1bb63328d1d8b85130d79f161
xdjyxu <xdjyxu@163.com>
2022-05-16 11:29:45 +0800

    add element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 const srcPath = resolve(__dirname, 'src')
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: resolve(srcPath, 'env'),
+  assetsInclude: [
+    '**/*.gltf',
+    '**/*.glb'
+  ],
   resolve: {
     alias: {
       '@/': `${srcPath}/`
@@ -17,15 +22,30 @@ export default defineConfig({
     modules: {
       localsConvention: 'camelCaseOnly'
     },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "~/styles/element/index.scss" as *;`
+      }
+    }
   },
   plugins: [
     vue(),
     AutoImport({
+      resolvers: [ElementPlusResolver()],
       imports: ['vue', 'vue-router', 'vue-i18n', '@vueuse/core'],
       dts: resolve(srcPath, 'auto-imports.d.ts')
     }),
     Components({
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'sass',
+        }),
+      ],
       dts: resolve(srcPath, 'components.d.ts')
     }),
-  ]
+  ],
+  build: {
+    rollupOptions: {
+    }
+  }
 })
